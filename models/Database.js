04eff5575690e85b233deea04b0a7fe02e5d2cce
MR_DIV <vshntvelip@gmail.com>
2026-05-26 18:38:51 +0530

    Update Database.js

--- models/Database.js
+++ models/Database.js
@@ -1,21 +1,13 @@
-// models/Database.js
 const { Schema, model } = require("mongoose");
 
 const databaseSchema = new Schema({
-  title: { 
-    type: String, 
-    required: true, 
-    trim: true 
-  },
+  title: { type: String, required: true, trim: true },
   category: { 
     type: String, 
     enum: ["about", "experience", "education", "achievement", "certificate", "other"],
     default: "other"
   },
-  description: { 
-    type: String, 
-    required: true 
-  },
+  description: { type: String, required: true },
   image: { type: String, default: "" },
   publicId: { type: String, default: "" },
   link: { type: String, default: "" },
